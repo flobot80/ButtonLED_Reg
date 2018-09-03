7f5c724eda7582d39231f41339f4e04bbc4b0944
Leyla Nazhandali <leyla@vt.edu>
2018-09-03 15:26:50 -0400

    user guide details

--- ButtonLED_Reg_main.c
+++ ButtonLED_Reg_main.c
@@ -5,15 +5,15 @@
 
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 
-// According to the schematics on page 37 of MCU datasheet,
+// According to the schematics on page 37 of the Launchpad user guide,
 // left button (button 1) is connected to pin 1 (bit1)
 #define LEFT_BUTTON BIT1
 
-// According to the schematics on page 37 of MCU datasheet,
+// According to the schematics on page 37 of the Launchpad user guide,
 // left LED (LED 1) is connected to pin 0 (bit0)
 #define LEFT_LED BIT0
 
-// According to the schematics on page 37 of MCU datasheet,
+// According to the schematics on page 37 of the Launchpad user guide,
 // When a button is pressed, it is grounded (logic 0)
 #define PRESSED 0
 
@@ -43,14 +43,14 @@ void initialize()
     WDT_A_hold(WDT_A_BASE);
 
     // step 2: Initializing LED1
-    // According to the table on page 678 of MCU User guide,
+    // According to table 12.1 on page 678 of MSP432 User guide,
     // to create an output, all you need is to write a 1 to PxDIR for the specific bit you want
     // A common mistake is to write P1DIR = LEFT_LED instead of P1DIR |= LEFT_LED;
     P1DIR |= LEFT_LED;  // Same as P1DIR = P1DIR | LEFT_LED;
 
 
     // step 3: Initializing S1 (switch 1 or button 1)
-    // According to the table on page 678 of MCU User guide,
+    // According to the table on page 678 of MSP432 User guide,
     // to create an input with pull-up resistor, you need to do three things
 
     // step 3.1: write a 0 to PxDIR for the specific bit you want
